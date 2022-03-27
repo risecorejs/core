c35091df3b520fc56eba6b7d0e86a768377f0dff
amenov <amenov.abdulsalam@gmail.com>
2022-03-27 22:30:01 +0600

    update

--- register/middleware.js
+++ register/middleware.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const rateLimit = require('express-rate-limit')
 const cors = require('cors')
-const only = require('@risecorejs/only')
+const only = require('@risecorejs/only/middleware')
 const validator = require('@risecorejs/validator')
 const whereBuilder = require('@risecorejs/where-builder/middleware')
 
