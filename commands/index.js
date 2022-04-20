ee8298ce8df5e9e07e96503335ff0f13e12773bd
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-04-20 13:42:33 +0600

    fix argv[2] -> argv[1]

--- commands/index.js
+++ commands/index.js
@@ -8,7 +8,7 @@ module.exports = {
     controller: (argv) => makers.controller(argv[1], argv[2]),
     model: (argv) => makers.model(argv[1]),
     docs: (argv) => makers.docs(argv[1], argv[2]),
-    routes: (argv) => makers.routes(argv[2]),
+    routes: (argv) => makers.routes(argv[1]),
     entity: require('./make-entity'),
     migrations: () => require('@risecorejs/make-migrations')
   }
