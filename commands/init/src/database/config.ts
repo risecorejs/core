b9f7381256538de6d4fb97b0d2a3b481bd82f87e
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-09-09 14:12:27 +0600

    update to ts

--- commands/init/src/database/config.ts
+++ commands/init/src/database/config.ts
@@ -2,12 +2,16 @@ require('dotenv').config()
 
 import { env } from '@risecorejs/helpers'
 
-module.exports = {
-  dialect: env('DB_DIALECT'),
-  host: env('DB_HOST'),
-  port: env('DB_PORT'),
-  database: env('DB_NAME'),
-  username: env('DB_USERNAME'),
-  password: env('DB_PASSWORD'),
-  logging: ['1', 'true'].includes(env('DB_LOGGING'))
+const mode = env('NODE_ENV', 'development')
+
+export = {
+  [mode]: {
+    dialect: env('DB_DIALECT'),
+    host: env('DB_HOST'),
+    port: env('DB_PORT'),
+    database: env('DB_NAME'),
+    username: env('DB_USERNAME'),
+    password: env('DB_PASSWORD'),
+    logging: ['1', 'true'].includes(env('DB_LOGGING'))
+  }
 }
